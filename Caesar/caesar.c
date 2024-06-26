6b374681881033097050ea4dc985732e88894058
AFlyingNut <william@ponbus.com>
2024-06-26 14:39:41 -0700

    Update caesar.c

--- Caesar/caesar.c
+++ Caesar/caesar.c
@@ -8,26 +8,14 @@
 const char uppercase[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 const char lowercase[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
+bool only_digits(string input);
+
 int main(int argc, string argv[]) {
     // Look for exactly 2 arguments
-    if (argc != 2)
+    if ((argc != 2) || !(only_digits(argv[1])))
     {
-        printf("Command Usage, %s [INT]\n", argv[0]);
+        printf("Usage, %s key\n", argv[0]);
         return 1;
-    }
-
-    // Check for if there are any non-digits in the arguments
-    for (int i = 0; i < strlen(argv[1]); i++)
-    {
-        if (isdigit(argv[1][i]))
-        {
-            continue;
-        }
-        else
-        {
-            printf("Command Usage, %s [INT]\n", argv[0]);
-            return 1;
-        }
     }
 
     string sentence = get_string("Text: ");
@@ -58,4 +46,17 @@ int main(int argc, string argv[]) {
     }
 
     printf("%s\n", ciphertext);
+}
+
+bool only_digits(string input)
+{
+    int non_digits = 0;
+    for (int i = 0; i < strlen(input); i++)
+    {
+        if (!(isdigit(input[i])))
+        {
+            return false;
+        }
+    }
+    return true;
 }
